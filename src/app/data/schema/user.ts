0603244b356864df554194343eafc27319857b8f
AmauryMichel <56653816+AmauryMichel@users.noreply.github.com>
2026-01-13 12:27:16 +0100

    refactor(user): store user entity in localstorage

--- src/app/data/schema/user.ts
+++ src/app/data/schema/user.ts
@@ -1,5 +1,10 @@
 export class User {
     id?: number;
-    username: string;
-    password: string;
+    username?: string;
+    password?: string;
+
+    constructor(id: number, username: string) {
+        this.id = id
+        this.username = username
+    }
 }
--- src/app/shared/services/authentification-service.ts
+++ src/app/shared/services/authentification-service.ts
@@ -25,15 +25,15 @@ export class AuthentificationService {
   setLoggedIn(token: string, username: string) {  
     let splitToken = token.split("!")
     localStorage.setItem('token', splitToken[1])
-    localStorage.setItem('userId', splitToken[0])
-    localStorage.setItem('username', username)
+
+    let user = new User(parseInt(splitToken[0]), username)
+    localStorage.setItem('user', JSON.stringify(user))
     this.isAuthenticated.next(true);
   }
 
   setLoggedOut() {
     localStorage.removeItem('token')
-    localStorage.removeItem('userId')
-    localStorage.removeItem('username')
+    localStorage.removeItem('user')
     this.isAuthenticated.next(false);
   }
 }
